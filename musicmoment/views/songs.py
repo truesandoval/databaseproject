9cf075e23e1aaa968037dde0eb8e960bac892109
kseeram1 <44853294+kseeram1@users.noreply.github.com>
2020-04-11 16:24:02 -0700

    Updated files

--- musicmoment/views/songs.py
+++ musicmoment/views/songs.py
@@ -17,11 +17,6 @@ def songs(conn):
 # def get_all_boats_by_popularity(conn):
 #     return execute(conn, "SELECT b.bid, b.name, b.color, count(v.bid) AS number_of_voyages FROM (Voyages As v INNER JOIN Boats AS b ON v.bid = b.bid) GROUP BY v.bid ORDER BY count(v.bid) desc")
 #
-def add_a_sugg(conn, name, mood, song_name, artist, url, comment):
-    if name == None or mood == None or song_name == None or artist == None or url == None or comment == None or name == "" or mood == "" or song_name == "" or artist == "" or url == "" or comment == "":
-        raise Exception
-    return execute(conn, "INSERT INTO Suggestions(name,color) VALUES (:name,:color) ", {'name': name, 'color': color} )
-
 
 def views(bp):
     @bp.route("/songs")
@@ -29,26 +24,6 @@ def views(bp):
         with get_db() as conn:
             rows = songs(conn)
         return render_template("table.html", name="Songs", rows=rows)
-
-    @bp.route("/suggestions/add")
-    def add_sugg_page():
-        return render_template("form_sugg.html")
-
-    @bp.route("/suggestions/add/submit", methods=['POST'])
-    def _add_a_sugg():
-        with get_db() as conn:
-            name = request.form['name']
-            mood = request.form['name']
-            song_name = request.form['song_name']
-            artist = request.form['artist']
-            url = request.form['url']
-            comment = request.form['comment']
-            try:
-                add_a_sugg(conn, name, color)
-            except Exception:
-                return render_template("form_error.html", errors=["Your record was not added. Check your inputs."])
-            rows = boats(conn)
-        return render_template("table.html", name="%s Boat Added" % name, rows=rows)
 
     # @bp.route("/boats/sailed-by")
     # def _get_all_boats_from_sailor_name():
--- musicmoment/views/suggestions.py
+++ musicmoment/views/suggestions.py
@@ -6,28 +6,55 @@ from flask import request
 from musicmoment.db import get_db, execute
 import datetime
 
-def voyages(conn):
-    return execute(conn, "SELECT v.sid, v.bid, v.date_of_voyage FROM Voyages AS v")
+
+def suggestions(conn):
+    return execute(conn, "SELECT * FROM Suggestions")
+
+def add_a_sugg(conn, name, mood, song_name, artist, url, comment):
+    if name == None or mood == None or song_name == None or artist == None or url == None or comment == None or name == "" or mood == "" or song_name == "" or artist == "" or url == "" or comment == "":
+        raise Exception
+    return execute(conn, "INSERT INTO Suggestions(username,songname,artist,url,moodname, comment) VALUES (:name,:songname,:artist,:url,:moodname,:comment) ", {'name': name, 'songname': song_name, 'artist':artist, 'url':url, 'moodname':mood, 'comment':comment} )
 
 def add_a_voyage(conn, sid, bid, date_of_voyage):
     try:
         datetime.datetime.strptime(date_of_voyage, '%Y-%m-%d')
     except ValueError:
         raise Exception
-    return execute(conn, "INSERT INTO Voyages(sid, bid, date_of_voyage) VALUES (:sid,:bid,:date_of_voyage)", {'sid': sid, 'bid': bid, 'date_of_voyage': date_of_voyage } )
+    return execute(conn, "INSERT INTO Voyages(sid, bid, date_of_voyage) VALUES (:sid,:bid,:date_of_voyage)", {'sid': sid, 'bid': bid, 'date_of_voyage': date_of_voyage})
+
 
 def views(bp):
-    @bp.route("/voyages")
-    def _voyages():
+    @bp.route("/suggestions")
+    def _suggestions():
         with get_db() as conn:
-            rows = voyages(conn)
-        return render_template("table.html", name="voyages", rows=rows)
+            rows = suggestions(conn)
+        return render_template("table.html", name="suggestions", rows=rows)
+
+    @bp.route("/suggestions/add")
+    def add_sugg_page():
+        return render_template("form_sugg.html")
+
+    @bp.route("/suggestions/add/submit", methods=['POST'])
+    def _add_a_sugg():
+        with get_db() as conn:
+            name = request.form['name']
+            mood = request.form['name']
+            song_name = request.form['song_name']
+            artist = request.form['artist']
+            url = request.form['url']
+            comment = request.form['comment']
+            try:
+                add_a_sugg(conn, name, color)
+            except Exception:
+                return render_template("form_error.html", errors=["Your record was not added. Check your inputs."])
+            rows = boats(conn)
+        return render_template("table.html", name="%s Boat Added" % name, rows=rows)
 
     @bp.route("/voyages/add")
     def add_voyage_page():
         return render_template("form_voyage.html")
 
-    @bp.route("/voyages/add/submit", methods = ['POST'])
+    @bp.route("/voyages/add/submit", methods=['POST'])
     def _add_a_voyage():
         with get_db() as conn:
             sid = request.form['sid']
@@ -36,6 +63,6 @@ def views(bp):
             try:
                 add_a_voyage(conn, sid, bid, date_of_voyage)
             except Exception:
-                return render_template("form_error.html", errors = ["Your record was not added. Check your inputs."])
+                return render_template("form_error.html", errors=["Your record was not added. Check your inputs."])
             rows = voyages(conn)
         return render_template("table.html", name="Voyage Added", rows=rows)
