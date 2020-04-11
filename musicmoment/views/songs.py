82ea33cb2d9e1ed29949329286e9593dc6ef89b9
kseeram1 <44853294+kseeram1@users.noreply.github.com>
2020-04-11 16:09:56 -0700

    Suggestions HTML
    Added suggestion HTML

--- musicmoment/views/songs.py
+++ musicmoment/views/songs.py
@@ -7,8 +7,9 @@ from flask import escape
 
 from musicmoment.db import get_db, execute
 
+
 def songs(conn):
-    return execute(conn, "SELECT s.sid, s.mid, s.name, s.artist, s.url FROM Songs AS s")
+    return execute(conn, "SELECT * FROM Songs")
 
 # def get_all_boats_from_sailor_name(conn, s_name):
 #     return execute(conn, "SELECT DISTINCT b.bid, b.name, b.color FROM ((Sailors AS s INNER JOIN Voyages As v ON s.sid = v.sid) INNER JOIN Boats AS b ON v.bid = b.bid) WHERE s.name = :s_name", {'s_name': s_name})
@@ -16,10 +17,11 @@ def songs(conn):
 # def get_all_boats_by_popularity(conn):
 #     return execute(conn, "SELECT b.bid, b.name, b.color, count(v.bid) AS number_of_voyages FROM (Voyages As v INNER JOIN Boats AS b ON v.bid = b.bid) GROUP BY v.bid ORDER BY count(v.bid) desc")
 #
-# def add_a_boat(conn, name, color):
-#     if name == None or color == None or name == "" or color == "":
-#         raise Exception
-#     return execute(conn, "INSERT INTO Boats(name,color) VALUES (:name,:color) ", {'name': name, 'color': color} )
+def add_a_sugg(conn, name, mood, song_name, artist, url, comment):
+    if name == None or mood == None or song_name == None or artist == None or url == None or comment == None or name == "" or mood == "" or song_name == "" or artist == "" or url == "" or comment == "":
+        raise Exception
+    return execute(conn, "INSERT INTO Suggestions(name,color) VALUES (:name,:color) ", {'name': name, 'color': color} )
+
 
 def views(bp):
     @bp.route("/songs")
@@ -27,6 +29,26 @@ def views(bp):
         with get_db() as conn:
             rows = songs(conn)
         return render_template("table.html", name="Songs", rows=rows)
+
+    @bp.route("/suggestions/add")
+    def add_sugg_page():
+        return render_template("form_sugg.html")
+
+    @bp.route("/suggestions/add/submit", methods=['POST'])
+    def _add_a_sugg():
+        with get_db() as conn:
+            name = request.form['name']
+            mood = request.form['name']
+            song_name = request.form['song_name']
+            artist = request.form['artist']
+            url = request.form['url']
+            comment = request.form['comment']
+            try:
+                add_a_sugg(conn, name, color)
+            except Exception:
+                return render_template("form_error.html", errors=["Your record was not added. Check your inputs."])
+            rows = boats(conn)
+        return render_template("table.html", name="%s Boat Added" % name, rows=rows)
 
     # @bp.route("/boats/sailed-by")
     # def _get_all_boats_from_sailor_name():
