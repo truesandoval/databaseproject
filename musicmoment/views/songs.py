77a0a92bb77d0264b3ec74efacae410dddc5dff2
Tanjin Prity <37024552+tanjinprity4@users.noreply.github.com>
2020-04-18 14:25:57 -0700

    Adding URL to songs mood

--- musicmoment/views/songs.py
+++ musicmoment/views/songs.py
@@ -12,7 +12,7 @@ def songs(conn):
     return execute(conn, "SELECT s.sid, s.mid, s.name, s.artist, s.url, s.explicit FROM Songs AS s")
     
 def songmood(conn):
-    return execute(conn, "SELECT songmood.sid, moods.mood, songs.name FROM Songs, Moods, SongMood WHERE songs.sid = songmood.sid and moods.mid = songmood.mid")
+    return execute(conn, "SELECT songmood.sid, moods.mood, songs.name, songs.url FROM Songs, Moods, SongMood WHERE songs.sid = songmood.sid and moods.mid = songmood.mid")
 
 # def get_all_boats_from_sailor_name(conn, s_name):
 #     return execute(conn, "SELECT DISTINCT b.bid, b.name, b.color FROM ((Sailors AS s INNER JOIN Voyages As v ON s.sid = v.sid) INNER JOIN Boats AS b ON v.bid = b.bid) WHERE s.name = :s_name", {'s_name': s_name})
